220b02ffb479e49bb32cdd47d3a0c9eecd5d9a66
FuZiYang <ditto1023@outlook.com>
2025-01-22 09:45:53 +0800

    feat: 初始化 Jeepaycopy 项目
    - 创建项目结构和基本配置
    - 添加 Spring Boot 依赖- 设置 Maven 构建配置- 创建 Git 属性和忽略文件

---
+++ src/main/java/com/jeequan/jeepaycopy/JeepaycopyApplication.java
@@ -0,0 +1,13 @@
+package com.jeequan.jeepaycopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepaycopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeepaycopyApplication.class, args);
+    }
+
+}
